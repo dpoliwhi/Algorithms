1ad51de02ef5cfffef45ee56f33f9fd2485b6d3a
dpoliwhi <evdidenko93@gmail.com>
2025-07-09 00:01:37 +0700

    27 task done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 //        task167();
 //        task344();
-        task1();
+//        task1();
+        task27();
     }
 
     private static void task167() {
@@ -29,4 +30,16 @@ public class Main {
         TwoSum_1 task1 = new TwoSum_1();
         System.out.println(Arrays.toString(task1.twoSum(nums, target)));
     }
+
+    private static void task27() {
+//        int[] nums = {3,2,2,3};
+//        int target = 3;
+//        int[] nums = {0,1,2,2,3,0,4,2};
+//        int target = 2;
+        int[] nums = {1};
+        int target = 1;
+        RemoveElements_27 task27 = new RemoveElements_27();
+        System.out.println(task27.removeElement(nums, target));
+    }
+
 }
